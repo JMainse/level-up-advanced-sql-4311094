bcc9263dd8815d590f1f27e0eeb2081c27db18e4
JMainse <57804160+JMainse@users.noreply.github.com>
2024-06-03 19:04:16 +0000

    My Solution for 01-02.

--- my_solutions/01-02.sql
+++ my_solutions/01-02.sql
@@ -1,1 +1,9 @@
 /*Find Salespeople who have zero sales*/
+--Sales Person
+
+SELECT *
+FROM employee emp
+LEFT JOIN sales sal ON emp.employeeId = sal.employeeId
+WHERE sal.salesAmount IS NULL
+AND emp.title = 'Sales Person'
+;
